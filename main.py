fbcc058f653ddab7521a9176d1b1771c577e5318
Gadila Shashank Reddy <shashankgadila@gmail.com>
2018-08-10 20:41:29 +0530

    Add command line arguments and update readme.

--- main.py
+++ main.py
@@ -9,59 +9,62 @@ CRONTASK
 @kaushalvivek
 '''
 
+# FILL THIS UP OTHERWISE THE SCRIPT WON'T RUN
+#################################################
+# Login details of any gmail account
+bot_mail = ''
+password = ''
+#################################################
+
+
+import argparse
 from bs4 import BeautifulSoup as bs
 import requests
 import smtplib
 import time
 
-####################################
 
-# Enter before execution:
+parser = argparse.ArgumentParser()
+parser.add_argument("-m", nargs="?", help="Name of movie to track", type=str)
 
-# Name of the movie to be searched
-mov = ''
+parser.add_argument("-l", nargs="?", help="Location where movie is tracked",
+                    type=str)
 
-# City where you want to search for ticket availability
-loc = ''
+parser.add_argument("email", help="Email address to notify if tickets are \
+                    available", type=str)
+args = parser.parse_args()
 
-# Login details of any gmail account
-bot_mail = ''
-password =''
+if args.m and args.l and args.email and bot_mail and password:
+    pass
+else:
+    print("Enter all the arguments\n")
+    print(parser.print_help())
+    quit()
 
-# Email address where you want to be notified
-target_mail = ''
 
-####################################
-
-# def input_movie():
-#     movie = input('Enter movie to lookout for: ')
-#     return movie
-    
-# def input_location():
-#     location = input('Enter city to search in: ')
-#     return location
-
-def search(mov,loc):
+def search(mov, loc):
     req = requests.get('https://www.google.co.in/search?q=bms '+loc+' '+mov)
-    soup = bs(req.text,'html.parser')
+    soup = bs(req.text, 'html.parser')
     tags = soup.findAll('h3')
-    for i in range (3):
-        analyse(tags[i].a.attrs['href'],mov,loc)
+    for i in range(3):
+        analyse(tags[i].a.attrs['href'], mov, loc)
 
-def mail (mov,loc):
+
+def mail(mov, loc):
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
     server.login(bot_mail, password)
-    SUBJECT = "bms_alert "+mov+" "+loc
-    TEXT = "Hi Human,\n\nTickets for "+mov+" are now on sale in "+loc+".\n Cheers!\n\nYours,\nLab PC"
-    msg = 'Subject: {}\n\n{}'.format(SUBJECT, TEXT) 
-    server.sendmail(bot_mail, target_mail, msg)
+    SUBJECT = "bms_alert " + mov + " " + loc
+    TEXT = "Hi Human, \n\nTickets for {} are for sale in {}. Cheers!\n\nYours, \nLab PC".format(mov, loc)
+    msg = 'Subject: {}\n\n{}'.format(SUBJECT, TEXT)
+    server.sendmail(bot_mail, args.email, msg)
     server.quit()
 
-def analyse(link,mov,loc):
+
+def analyse(link, mov, loc):
     if 'buytickets' in link:
-        mail(mov,loc)
+        mail(mov, loc)
         quit()
-    
-link = search(mov,loc)
-    
+
+
+link = search(args.m, args.l)
